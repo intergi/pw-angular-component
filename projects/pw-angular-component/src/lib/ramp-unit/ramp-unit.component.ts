0716c86d82265f1db65410a25a80bf89431041b1
Nico Valdez <nicomvaldez@gmail.com>
2022-09-28 17:32:06 -0300

    ramp unit component tweaks

--- projects/pw-angular-component/src/lib/ramp-unit/ramp-unit.component.ts
+++ projects/pw-angular-component/src/lib/ramp-unit/ramp-unit.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ramp-unit',
   templateUrl: './ramp-unit.component.html',
   styleUrls: ['./ramp-unit.component.css']
 })
+
 export class RampUnitComponent implements OnInit {
+
+  @Input() type: string | undefined;
+  @Input() selectorId: string | undefined;
+  @Input() cssClass: string | undefined;
 
   constructor() {
   }
@@ -15,8 +20,8 @@ export class RampUnitComponent implements OnInit {
     window.ramp.que.push(() => {
       window.ramp.addUnits([
         {
-          type: 'leaderboard_atf',
-          selectorId: 'leaderboard_atf'
+          type: this.type,
+          selectorId: this.selectorId
         }
       ]).catch((e: any) => {
 
